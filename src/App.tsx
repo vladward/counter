65bb8a7bff2bac19f8e208a1fdf5105947056cb7
vladward <vladward13@gmail.com>
2021-11-18 17:34:23 +0300

    feat: add useEffect with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { Button } from './Components/Button/Button';
+import {Button} from './Components/Button/Button';
 
 const App = () => {
     const [startValue, setStartValue] = useState(0)
     const [maxValue, setMaxValue] = useState(0)
 
-    const incHandler = () => {
-        setStartValue(startValue + 1)
-        setFromLocStorage()
-    }
-    const resetHandler = () => {
-      setStartValue(0)
-        setFromLocStorage()
-    }
-
-    const disabledInc = startValue === 5
-    const disabledRes = startValue === 0
-
-    const setFromLocStorage = () => {
-        localStorage.setItem('counterValue', JSON.stringify(startValue))
-    }
-    const getFromLocStorage = () => {
+    useEffect(() => {
         let item = localStorage.getItem('counterValue')
         if (item) {
             let newValue = JSON.parse(item)
             setStartValue(newValue)
         }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(startValue))
+    }, [startValue])
+
+    const incHandler = () => {
+        setStartValue(startValue + 1)
     }
+    const resetHandler = () => {
+        localStorage.clear()
+        setStartValue(0)
+    }
+
+    const disabledInc = startValue === 5
+    const disabledRes = startValue === 0
+
 
     return (
         <div className="App">
@@ -35,8 +36,8 @@ const App = () => {
                 <h1>{startValue}</h1>
             </div>
             <div className="button-container">
-                <Button callback={incHandler} disabled={disabledInc} name={'inc'} />
-                <Button callback={resetHandler} disabled={disabledRes} name={'reset'} />
+                <Button callback={incHandler} disabled={disabledInc} name={'inc'}/>
+                <Button callback={resetHandler} disabled={disabledRes} name={'reset'}/>
             </div>
         </div>
     );
