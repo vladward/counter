2092befcd9532c8d34e171761fd5573d5294772c
vladward <vladward13@gmail.com>
2021-11-18 17:15:58 +0300

    ref: Button component refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React, {useState} from 'react';
 import './App.css';
+import { Button } from './Components/Button/Button';
 
 const App = () => {
-    const [value, setValue] = useState(0)
+    const [startValue, setStartValue] = useState(0)
+    const [maxValue, setMaxValue] = useState(0)
 
     const incHandler = () => {
-        setValue(value + 1)
+        setStartValue(startValue + 1)
+        setFromLocStorage()
+    }
+    const resetHandler = () => {
+      setStartValue(0)
+        setFromLocStorage()
     }
 
-    const resetHandler = () => {
-      setValue(0)
+    const disabledInc = startValue === 5
+    const disabledRes = startValue === 0
+
+    const setFromLocStorage = () => {
+        localStorage.setItem('counterValue', JSON.stringify(startValue))
+    }
+    const getFromLocStorage = () => {
+        let item = localStorage.getItem('counterValue')
+        if (item) {
+            let newValue = JSON.parse(item)
+            setStartValue(newValue)
+        }
     }
 
     return (
         <div className="App">
-            <h1>LocalStorage</h1>
-
-            <h2>0</h2>
-            <button onClick={incHandler}>inc</button>
-            <button onClick={resetHandler}>reset</button>
+            <div className="value-container">
+                <h1>{startValue}</h1>
+            </div>
+            <div className="button-container">
+                <Button callback={incHandler} disabled={disabledInc} name={'inc'} />
+                <Button callback={resetHandler} disabled={disabledRes} name={'reset'} />
+            </div>
         </div>
     );
 }
