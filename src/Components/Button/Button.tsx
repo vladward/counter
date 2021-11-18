139f3c39fd968792e6ee71b0034e69b00c97e472
vladward <vladward13@gmail.com>
2021-11-18 17:15:03 +0300

    ref: Button component refactor

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,5 +1,14 @@
 import React from "react"
 
-export const Button = () => {
-    return <button></button>
+type ButtonPropsType = {
+    callback: () => void
+    name: string
+    disabled?: boolean
+}
+
+export const Button = ({callback, name, disabled}: ButtonPropsType) => {
+    const onClickHandler = () => {
+        callback()
+    }
+    return <button onClick={onClickHandler} disabled={disabled}>{name}</button>
 }
