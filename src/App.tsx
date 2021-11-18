4a78f093878a5dbab036043fbe0cd1fcc2ee31c0
vladward <vladward13@gmail.com>
2021-11-18 15:31:57 +0300

    feat: add increment and reset buttons and state with for them

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
+    const [value, setValue] = useState(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const resetHandler = () => {
+      setValue(0)
+    }
+
     return (
         <div className="App">
             <h1>LocalStorage</h1>
 
-    </div>
-  );
+            <h2>0</h2>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={resetHandler}>reset</button>
+        </div>
+    );
 }
 
 export default App;
