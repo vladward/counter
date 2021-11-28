e3f7235b06775c055f1fb55b1fca3ebb64c0ee52
vladward <vladward13@gmail.com>
2021-11-28 12:58:56 +0300

    feat: app refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,76 @@ import {Button} from './Components/Button/Button';
 const App = () => {
     const [minValue, setMinValue] = useState(0)
     const [maxValue, setMaxValue] = useState(0)
+    const [endMinValue, setEndMinValue] = useState(0)
+    const [endMaxValue, setEndMaxValue] = useState(0)
+    const [changeMode, setChangeMode] = useState(false)
+    const [incorrect, setIncorrect] = useState(false)
 
     useEffect(() => {
         let item = localStorage.getItem('counterValue')
+        let itemMaxValue = localStorage.getItem('counterMaxValue')
+        let itemEndMinValue = localStorage.getItem('counterEndMinValue')
+        let itemEndMaxValue = localStorage.getItem('counterEndMaxValue')
         if (item) {
             let newValue = JSON.parse(item)
             setMinValue(newValue)
         }
+        if (itemMaxValue) {
+            let newValue = JSON.parse(itemMaxValue)
+            setMaxValue(newValue)
+        }
+        if (itemEndMinValue) {
+            let newValue = JSON.parse(itemEndMinValue)
+            setEndMinValue(newValue)
+        }
+        if (itemEndMaxValue) {
+            let newValue = JSON.parse(itemEndMaxValue)
+            setEndMaxValue(newValue)
+        }
+        setChangeMode(true)
     }, [])
 
     useEffect(() => {
         localStorage.setItem('counterValue', JSON.stringify(minValue))
-    }, [minValue])
+        localStorage.setItem('counterMaxValue', JSON.stringify(maxValue))
+        localStorage.setItem('counterEndMinValue', JSON.stringify(endMinValue))
+        localStorage.setItem('counterEndMaxValue', JSON.stringify(endMaxValue))
+    }, [minValue, maxValue, endMinValue, endMaxValue])
 
     const incHandler = () => {
         setMinValue(minValue + 1)
     }
     const resetHandler = () => {
         localStorage.clear()
-        setMinValue(0)
+        setMinValue(endMinValue)
+    }
+
+    const setEditMode = () => {
+        setChangeMode(false)
     }
 
     const minOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         let value = e.currentTarget.value
         setMinValue(Number(value))
+        setEndMinValue(Number(value))
+        if (Number(value) >= endMaxValue) {
+            setIncorrect(true)
+        } else {
+            setIncorrect(false)
+            setChangeMode(true)
+        }
     }
-
-    const disabledInc = minValue === 5
-    const disabledRes = minValue === 0
-
+    const maxOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        let value = e.currentTarget.value
+        setMaxValue(Number(value))
+        setEndMaxValue(Number(value))
+        if (Number(value) <= endMinValue) {
+            setIncorrect(true)
+        } else {
+            setIncorrect(false)
+            setChangeMode(true)
+        }
+    }
 
     return (
         <div className="App">
@@ -45,22 +86,33 @@ const App = () => {
                             <div><span>min value</span></div>
                         </div>
                         <div className="value-inputs">
-                            <div><input value={maxValue} type="number"/></div>
-                            <div><input value={minValue} onChange={minOnChangeHandler} type="number"/></div>
+                            <div><input value={endMaxValue}
+                                        style={incorrect ? {background: "red"} : {}}
+                                        onChange={maxOnChangeHandler}
+                                        type="number"/>
+                            </div>
+                            <div><input value={endMinValue}
+                                        style={incorrect ? {background: "red"} : {}}
+                                        onChange={minOnChangeHandler}
+                                        type="number"/>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="button-container">
-                    <Button callback={incHandler} disabled={disabledInc} name={'set'}/>
+                    <Button callback={setEditMode} disabled={!changeMode || incorrect} name={'set'}/>
                 </div>
             </div>
             <div className="container">
                 <div className="value-container">
-                    <h1>{minValue}</h1>
+                    {incorrect
+                    ?   <p style={{color: "red"}}>Incorrect value!</p>
+                    :   <p style={minValue === maxValue ? {color: "red", fontSize: "32px", fontWeight: 900} : {}}>{!changeMode ? minValue : "enter values and press 'set'"}</p>
+                    }
                 </div>
                 <div className="button-container">
-                    <Button callback={incHandler} disabled={disabledInc} name={'inc'}/>
-                    <Button callback={resetHandler} disabled={disabledRes} name={'reset'}/>
+                    <Button callback={incHandler} disabled={changeMode || (minValue === endMaxValue)} name={'inc'}/>
+                    <Button callback={resetHandler} disabled={changeMode || (minValue === endMinValue)} name={'reset'}/>
                 </div>
             </div>
         </div>
