5023a6e831273fd1a211fd5cf7ac31b48e4525bc
vladward <vladward13@gmail.com>
2021-12-15 16:18:46 +0300

    feat: import utils to work with localstorage & add store.subscribe

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,18 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {counterReducer} from "./counterReducer";
 import thunk from "redux-thunk";
+import {loadState, saveState} from "../utils/localstotorage-utils";
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, loadState(), applyMiddleware(thunk))
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    })
+})
+
 export type AppStateType = ReturnType<typeof rootReducer>
