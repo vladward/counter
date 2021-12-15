8258ad286a9c58239a5a90e928e690bec570aa14
vladward <vladward13@gmail.com>
2021-12-15 16:23:10 +0300

    ref: global refactoring App for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import {Button} from './Components/Button/Button';
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "./redux/store";
+import {
+    IncCounterValueAC,
+    SetChangeModeAC, SetIncorrectAC, SetIsCheckedAC,
+    SetMaxValueAC,
+    SetMinValueAC, SetNightModeAC, SetRadioValueAC,
+    SetValueAC
+} from "./redux/counterReducer"
+
+type CounterType = {
+    value: number,
+    minValue: number,
+    maxValue: number,
+    isChecked: boolean,
+    radioValue: string,
+    changeMode: boolean,
+    incorrect: boolean,
+    nightMode: boolean
+}
 
 const App = () => {
-    const [initValue, setInitValue] = useState(0)
-    const [minValue, setMinValue] = useState(0)
-    const [maxValue, setMaxValue] = useState(0)
-    const [isChecked, setIsChecked] = useState(false)
-    const [radioValue, setRadioValue] = useState('Off')
-    const [changeMode, setChangeMode] = useState(false)
-    const [incorrect, setIncorrect] = useState(false)
-    const [nightMode, setNightMode] = useState(true)
+
+    const counter = useSelector<AppStateType, CounterType>(state => state.counter)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         let itemMinValue = localStorage.getItem('counterMinValue')
@@ -18,95 +33,120 @@ const App = () => {
         let itemValue = localStorage.getItem('counterInitValue')
         if (itemMinValue) {
             let newValue = JSON.parse(itemMinValue)
-            setMinValue(+newValue)
+            dispatch(SetMinValueAC(+newValue))
+            //setMinValue(+newValue)
         }
         if (itemMaxValue) {
             let newValue = JSON.parse(itemMaxValue)
-            setMaxValue(+newValue)
+            dispatch(SetMaxValueAC(+newValue))
+            //setMaxValue(+newValue)
         }
         if (itemValue) {
             let newValue = JSON.parse(itemValue)
-            setInitValue(newValue)
+            dispatch(SetValueAC(+newValue))
+            //setInitValue(newValue)
         }
-        setChangeMode(false)
+        dispatch(SetChangeModeAC(false))
+        //setChangeMode(false)
     }, []) //1 time
 
     useEffect(() => {
-        if (isChecked) {
-            localStorage.setItem('counterMinValue', JSON.stringify(minValue))
-            localStorage.setItem('counterMaxValue', JSON.stringify(maxValue))
+        if (!counter.isChecked) {
+            localStorage.setItem('counterMinValue', JSON.stringify(counter.minValue))
+            localStorage.setItem('counterMaxValue', JSON.stringify(counter.maxValue))
         }
-        localStorage.setItem('counterInitValue', JSON.stringify(initValue))
-    }, [initValue])
+        localStorage.setItem('counterInitValue', JSON.stringify(counter.value))
+    }, [counter.value])
 
     const incHandler = () => {
-        setInitValue(initValue + 1)
+        dispatch(IncCounterValueAC())
+        //setInitValue(initValue + 1)
     }
     const resetHandler = () => {
-        if (!isChecked) {
+        if (counter.isChecked) {
             localStorage.clear()
-            setInitValue(0)
+            dispatch(SetValueAC(0))
+            //setInitValue(0)
             localStorage.setItem('counterInitValue', JSON.stringify(0))
         } else {
-            setInitValue(minValue ? minValue : 0)
+            dispatch(SetValueAC(counter.minValue ? counter.minValue : 0))
+            //setInitValue(minValue ? minValue : 0)
         }
     }
 
     const clearMinMaxStorage = () => {
-        if (isChecked) {
+        if (counter.isChecked) {
             localStorage.removeItem('counterMinValue')
             localStorage.removeItem('counterMaxValue')
-            setMaxValue(0)
-            setMinValue(0)
+            dispatch(SetMaxValueAC(0))
+            //setMaxValue(0)
+            dispatch(SetMinValueAC(0))
+            //setMinValue(0)
         }
     }
 
     const setEditMode = () => {
-        setChangeMode(false)
-        setInitValue(minValue)
+        dispatch(SetChangeModeAC(false))
+        //setChangeMode(false)
+        dispatch(SetValueAC(counter.minValue))
+        //setInitValue(minValue)
     }
 
     const minOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         let value = e.currentTarget.value
-        setMinValue(Number(value))
-        setInitValue(0)
-        if (Number(value) >= maxValue) {
-            setIncorrect(true)
+        dispatch(SetMinValueAC(Number(value)))
+        //setMinValue(Number(value))
+        dispatch(SetValueAC(0))
+        //setInitValue(0)
+        if (Number(value) >= counter.maxValue) {
+            dispatch(SetIncorrectAC(true))
+            //setIncorrect(true)
         } else {
-            setIncorrect(false)
-            setChangeMode(true)
+            dispatch(SetIncorrectAC(false))
+            //setIncorrect(false)
+            dispatch(SetChangeModeAC(true))
+            //setChangeMode(true)
         }
     }
     const maxOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         let value = e.currentTarget.value
-        setMaxValue(Number(value))
-        setInitValue(0)
-        if (Number(value) <= minValue) {
-            setIncorrect(true)
+        dispatch(SetMaxValueAC(Number(value)))
+        //setMaxValue(Number(value))
+        dispatch(SetValueAC(0))
+        //setInitValue(0)
+        if (Number(value) <= counter.minValue) {
+            dispatch(SetIncorrectAC(true))
+            //setIncorrect(true)
         } else {
-            setIncorrect(false)
-            setChangeMode(true)
+            dispatch(SetIncorrectAC(false))
+            //setIncorrect(false)
+            dispatch(SetChangeModeAC(true))
+            //setChangeMode(true)
         }
     }
-    let incorrectValue = incorrect ? "Incorrect value!" : ""
+    let incorrectValue = counter.incorrect ? "Incorrect value!" : ""
 
     const onChangeRadioHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setRadioValue(e.currentTarget.value)
-        setIsChecked(!isChecked)
-        setChangeMode(false)
+        dispatch(SetRadioValueAC(e.currentTarget.value))
+        //setRadioValue(e.currentTarget.value)
+        dispatch(SetIsCheckedAC(!counter.isChecked))
+        //setIsChecked(!isChecked)
+        dispatch(SetChangeModeAC(false))
+        //setChangeMode(false)
         clearMinMaxStorage()
     }
 
     const dayNightHandler = () => {
         // let value = e.currentTarget.checked
-        setNightMode(!nightMode)
+        dispatch(SetNightModeAC(!counter.nightMode))
+        //setNightMode(!nightMode)
     }
 
     return (
-        <div className="App" style={!nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
+        <div className="App" style={!counter.nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
             <div className="night-container">
                 <label>Night Mode</label>
-                <input type="checkbox" checked={!nightMode} onChange={dayNightHandler}/>
+                <input type="checkbox" checked={!counter.nightMode} onChange={dayNightHandler}/>
             </div>
             <div className="radio-container">
                 <label htmlFor="" >Edit mode</label>
@@ -116,20 +156,20 @@ const App = () => {
                            name="one"
                            value="Off"
                            className="input-radio"
-                           checked={radioValue === "Off"}
+                           checked={counter.radioValue === "Off"}
                            onChange={onChangeRadioHandler}/>
                     <label htmlFor="off">Off</label>
                     <input id="on"
                            type="radio"
                            name="one"
                            value="On"
-                           checked={radioValue === "On"}
+                           checked={counter.radioValue === "On"}
                            onChange={onChangeRadioHandler}/>
                     <label htmlFor="on">On</label>
                 </div>
             </div>
-            {radioValue === "On" && (
-                <div className="container" style={!nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
+            {counter.radioValue === "On" && (
+                <div className="container" style={!counter.nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
                     <div className="value-container">
                         <div className="value-items">
                             <div className="value-spans">
@@ -138,14 +178,14 @@ const App = () => {
                             </div>
                             <div className="value-inputs">
                                 <div>
-                                    <input value={maxValue}
-                                           style={incorrect ? {background: "red"} : {background: "gainsboro"}}
+                                    <input value={counter.maxValue}
+                                           style={counter.incorrect ? {background: "red"} : {background: "gainsboro"}}
                                            onChange={maxOnChangeHandler}
                                            type="number"/>
                                 </div>
                                 <div>
-                                    <input value={minValue}
-                                           style={incorrect ? {background: "red"} : {background: "gainsboro"}}
+                                    <input value={counter.minValue}
+                                           style={counter.incorrect ? {background: "red"} : {background: "gainsboro"}}
                                            onChange={minOnChangeHandler}
                                            type="number"/>
                                 </div>
@@ -153,24 +193,24 @@ const App = () => {
                         </div>
                     </div>
                     <div className="button-container">
-                        <Button callback={setEditMode} disabled={!changeMode || incorrect} name={'set'}/>
+                        <Button callback={setEditMode} disabled={!counter.changeMode || counter.incorrect} name={'set'}/>
                     </div>
                 </div>)}
-            <div className="container" style={!nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
+            <div className="container" style={!counter.nightMode ? {backgroundColor: "#121212" ,color: "white"} : {}}>
                 <div className="value-container">
-                    {incorrect
+                    {counter.incorrect
                         ? <h1 style={{color: "red"}}>{incorrectValue}</h1>
-                        : <h1 style={(initValue === maxValue && initValue !== 0) ? {
+                        : <h1 style={(counter.value === counter.maxValue && counter.value !== 0) ? {
                             color: "red",
                             fontSize: "32px",
                             fontWeight: 900
-                        } : {}}>{!changeMode ? initValue : "enter values and press 'set'"}</h1>
+                        } : {}}>{!counter.changeMode ? counter.value : "enter values and press 'set'"}</h1>
                     }
                 </div>
                 <div className="button-container">
-                    <Button callback={incHandler} disabled={isChecked && (changeMode || (initValue === maxValue))}
+                    <Button callback={incHandler} disabled={counter.isChecked && (counter.changeMode || (counter.value === counter.maxValue))}
                             name={'inc'}/>
-                    <Button callback={resetHandler} disabled={isChecked && (changeMode || (initValue === minValue))}
+                    <Button callback={resetHandler} disabled={counter.isChecked && (counter.changeMode || (counter.value === counter.minValue))}
                             name={'reset'}/>
                 </div>
             </div>
